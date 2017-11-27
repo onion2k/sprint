f5261df5faf9f63b11056b00b087804bd1160e2a
Chris <chris@usablehq.com>
2017-11-27 06:38:41 +0000

    Layout

--- src/App.js
+++ src/App.js
@@ -6,11 +6,45 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <a href="https://sprinter.io">Sprinter</a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <aside>
+            <nav>
+              <a href="">Project Settings</a>
+            </nav>
+            <section>
+              <h1>Sprint 1</h1>
+            </section>
+            <section>
+              <h1>Sprint 2</h1>
+            </section>
+            <section>
+              <h1>Sprint 3</h1>
+            </section>
+            <section>
+              <h1>Sprint 4</h1>
+            </section>
+          </aside>
+          <article>
+            <nav>
+              <a href="">Previous Sprint</a>
+              <a href="">Next Sprint</a>
+            </nav>
+            <section>
+              <h1>Sprint 1</h1>
+            </section>
+            <section>
+              <h1>Sprint 2</h1>
+            </section>
+            <section>
+              <h1>Sprint 3</h1>
+            </section>
+            <section>
+              <h1>Sprint 4</h1>
+            </section>
+          </article>
+        </main>
       </div>
     );
   }
