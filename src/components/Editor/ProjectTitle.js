7c501db2c0b6b3d6ca511b4d93fadaa9163fa10e
Chris Neale <chris@komododigital.co.uk>
2017-11-30 13:14:50 +0000

    Started form

--- src/components/Editor/ProjectTitle.js
+++ src/components/Editor/ProjectTitle.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ProjectTitle extends Component {
     render(){
         let className = this.props.title ? "" : "pending";
-        return (<h1 className={ className }>{ this.props.title || "___" }</h1>);
+        return (<h1 className={ className }>{ this.props.title || '____________' }</h1>);
     }
 }
 
--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -6,12 +6,20 @@ class Task extends Component {
         return (
             <section key={this.props.task.id}>
                 <div>
-                    <div className="Title"><label>Title</label> { this.props.task.title }</div>
-                    <div className="Type"><label>Type</label> { this.props.task.type }</div>
+                    <div className="Title">
+                        <label>Title</label><input type="text" value={ this.props.task.title } />
+                    </div>
+                    <div className="Type">
+                        <label>Type</label> { this.props.task.type }
+                    </div>
                 </div>
                 <div>
-                    <div className="Min"><label>Min</label> { this.props.task.min }</div>
-                    <div className="Max"><label>Max</label> { this.props.task.max }</div>
+                    <div className="Min">
+                        <label>Min</label><input type="text" value={ this.props.task.min } />
+                    </div>
+                    <div className="Max">
+                        <label>Max</label><input type="text" value={ this.props.task.max } />
+                    </div>
                 </div>
                 <p>{ this.props.task.comments || 'No comments' }</p>
             </section>
