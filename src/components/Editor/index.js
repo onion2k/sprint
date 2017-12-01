c028270282f5fe9b4870bc8022285ba36a21c0fe
Chris Neale <chris@komododigital.co.uk>
2017-12-01 16:42:28 +0000

    Layout

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './Editor.css';
 import firebase from '../../firebase.js';
-import { Button, Container, Divider, Grid, Header, Image, Menu, Segment, Statistic } from 'semantic-ui-react'
+import { Form, Button, Container, Divider, Grid, Header, Image, Menu, Segment, Statistic, Select } from 'semantic-ui-react'
 
 import Task from '../Task';
 import SprintPager from '../SprintPager';
+
+const options = [
+    { key: 'design', text: 'Design', value: 'design' },
+    { key: 'development', text: 'Development', value: 'development' },
+]
 
 class Editor extends Component {
 
@@ -13,7 +18,8 @@ class Editor extends Component {
         super();
 
         this.state = {
-            project: undefined,
+            project: '',
+            task: 'design',
             sprints: ['abcdef123','abcdef456','abcdef789','abcdef100'],
             sprint: {
                 'abcdef123': { title: 'Sprint 1', total: 75 },
@@ -34,6 +40,9 @@ class Editor extends Component {
             }
         };
 
+        this.handleChange = this.handleChange.bind(this);
+        this.updateTask = this.updateTask.bind(this);
+
     }
 
     componentWillMount() {
@@ -46,49 +55,77 @@ class Editor extends Component {
 
     }
 
+    updateTask(task){
+        let t = this.state.tasks;
+        let tc = t[this.props.match.params.sprint].find((t)=>{ return t.id === task.id});
+        tc = task;
+        this.setState({ tasks: t });
+    }
+
+    handleChange = (e) => {
+
+        this.setState({ project: e.target.value });
+
+    };
+
     render() {
 
         let tasks = this.state.tasks[this.props.match.params.sprint].map((task) => {
-            return <Task key={task.id} task={ task } />
+            return <Task key={task.id} task={ task } update={this.updateTask} />
         });
 
-        let min = this.state.tasks[this.props.match.params.sprint].reduce( function(a, b){ return a + b['min']; }, 0);
-        let avg = this.state.tasks[this.props.match.params.sprint].reduce( function(a, b){ return a + (b['min']+b['max'])/2; }, 0);
-        let max = this.state.tasks[this.props.match.params.sprint].reduce( function(a, b){ return a + b['max']; }, 0);
-
+        let a = 0;
+        let min = this.state.tasks[this.props.match.params.sprint].reduce( function(a, b){ return a + parseInt(b['min'], 10); }, 0);
+        let max = this.state.tasks[this.props.match.params.sprint].reduce( function(a, b){ return a + parseInt(b['max'], 10); }, 0);
+        let avg = this.state.tasks[this.props.match.params.sprint].reduce( function(a, b){ return a + ( (parseInt(b['min'], 10)+parseInt(b['max'], 10)) / 2 ); }, 0);
+        
         let cnt = avg - min;
         let risk = max / min;
         
         return (
-            <article className="Editor">
+            <article className="Editor" style={{ marginTop: '1.0rem'}}>
                 <Header as='h1' dividing>{ this.state.project }</Header>
+
                 <Statistic.Group widths='four'>
                     <Statistic>
                         <Statistic.Value>{ avg }</Statistic.Value>
                         <Statistic.Label>Hours Quoted</Statistic.Label>
                     </Statistic>
-
                     <Statistic>
                         <Statistic.Value>{ min }</Statistic.Value>
-                        <Statistic.Label>Scheduled</Statistic.Label>
+                        <Statistic.Label>Required Hours</Statistic.Label>
                     </Statistic>
-
                     <Statistic>
                         <Statistic.Value>{ cnt }</Statistic.Value>
                         <Statistic.Label>Contingency</Statistic.Label>
                     </Statistic>
-
                     <Statistic>
                         <Statistic.Value>{ Math.round(risk) }</Statistic.Value>
                         <Statistic.Label>Risk</Statistic.Label>
                     </Statistic>
                 </Statistic.Group>
 
+                <Header as='h2' dividing>Sprint Details</Header>
+
+                <Form style={{ backgroundColor: '#f8f8f8', padding: '15px', marginBottom: '15px', border: '1px solid #eee' }}>
+                    <Form.Field required>
+                        <label>Sprint Title</label>
+                        <input name='project' placeholder='What is it...' value={ this.state.project } onChange={ this.handleChange } />
+                    </Form.Field>
+                    <Form.Field name='type' required control={Select} label='Type' options={options} value={this.state.type} placeholder='Type' onChange={ this.handleChange } />
+                </Form>
+
                 <Header as='h2' dividing>Tasks</Header>
 
                 <div>
                     { tasks }
                 </div>
+
+                <Header as='h2' dividing>Risks</Header>
+
+                <Form style={{ backgroundColor: '#f8f8f8', padding: '15px', marginBottom: '15px', border: '1px solid #eee' }}>
+                    <Form.TextArea name='risks' label='Risks' value={this.state.risks} placeholder='What are the unknowns? Why might this sprint be hard?' onChange={ this.handleChange } />
+                </Form>
 
             </article>
         );
--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
-import { Divider, Button, Checkbox, Form, Input, Radio, Select, TextArea } from 'semantic-ui-react'
+import { Divider, Popup, Button, Checkbox, Form, Input, Radio, Select, TextArea } from 'semantic-ui-react'
 import './Task.css';
-
-const options = [
-    { key: 'design', text: 'Design', value: 'design' },
-    { key: 'development', text: 'Development', value: 'development' },
-]
 
 class Task extends Component {
 
-    handleChange = (e, { value }) => this.setState({ value })
+    constructor(props){
+        super(props);
+        this.state = {
+            task: props.task
+        }
+    }
+
+    handleChange = (e, { name, value }) => {
+        let task = this.state.task;
+        task[name] = value;
+        this.setState({ task: task });
+        this.props.update(task);
+    };
     
     render() {
+
+        const { task } = this.state;
+
         return (
-            <Form key={this.props.task.id}>
-                <Form.Field required width={16}>
-                    <label>Task</label>
-                    <input placeholder='What is it...' value={this.props.task.title} />
-                </Form.Field>
+            <Form key={task.id} style={{ backgroundColor: '#f8f8f8', padding: '15px 15px 0', marginBottom: '15px', border: '1px solid #eee' }}>
                 <Form.Group>
-                    <Form.Input required width={3} label='Minimum Hours' placeholder='3.5' value={this.props.task.min} />
-                    <Form.Input required width={3} label='Maximum Hours' placeholder='100' value={this.props.task.max} />
-                    <Form.Field required width={6} control={Select} label='Task type' options={options} placeholder='Design, or development, or...' />
-                    <Form.Button width={2} label='Delete'>Danger</Form.Button>
-                    <Form.Button width={2} label='Move'>Move</Form.Button>
+                    <Form.Field required width={8}>
+                        <label>Task Title</label>
+                        <input name='title' placeholder='What is it...' value={task.title} onChange={ this.handleChange } />
+                    </Form.Field>
+                    <Form.Input name='min' required width={3} label='Minimum Hours' placeholder='3.5' value={task.min} onChange={ this.handleChange } />
+                    <Form.Input name='max' required width={3} label='Maximum Hours' placeholder='100' value={task.max} onChange={ this.handleChange } />
+                    <Form.Field>
+                        <Popup
+                            trigger={<Button content='Delete' style={{ marginTop: '25px' }} />}
+                            content={<Button color='red' content='Are you sure?' />}
+                            on='click'
+                            position='top right'
+                        />
+                    </Form.Field>
                 </Form.Group>
-                <Divider section />
             </Form>
         );
     }
