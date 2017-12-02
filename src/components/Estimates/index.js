8a38bd06f172d4781e04d4dddd647450338816d7
Chris <chris@usablehq.com>
2017-12-02 18:05:22 +0000

    Tidying up

---
+++ src/components/Estimates/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Estimates.css';
+
+class Estimates extends Component {
+  render() {
+    return (
+        <article className="Estimates">
+            <div>
+            Estimates
+            </div>
+        </article>
+    );
+  }
+}
+
+export default Estimates;
---
+++ src/components/Templates/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Templates.css';
+
+class Templates extends Component {
+  render() {
+    return (
+        <article className="Templates">
+            <div>
+                Templates
+            </div>
+        </article>
+    );
+  }
+}
+
+export default Templates;
